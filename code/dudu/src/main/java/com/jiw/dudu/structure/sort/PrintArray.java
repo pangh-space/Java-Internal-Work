5317bc1dd1a61f8ac4416960fdb7c46bc3078018
pangh <kungfu_star@sina.com>
2022-11-11 20:47:33 +0800

    排序算法：冒泡排序

---
+++ code/dudu/src/main/java/com/jiw/dudu/structure/sort/PrintArray.java
@@ -0,0 +1,43 @@
+package com.jiw.dudu.structure.sort;
+
+import java.util.ArrayList;
+
+/**
+ * @Description 打印
+ * @Author pangh
+ * @Date 2022年11月11日
+ * @Version v1.0.0
+ */
+public class PrintArray {
+
+    public final static int[] SRC
+            = {86,39,77,23,32,45,58,63,93,4,37,22};
+
+    public static void print(int[] array){
+        for(int i :array){
+            System.out.print(i+"  ");
+        }
+        System.out.println("");
+    }
+
+    public static void printIndex(int[] array,int begin ,int end){
+        for(int i=begin;i<=end;i++){
+            System.out.print(array[i]+"  ");
+        }
+        System.out.println("");
+    }
+
+    public static void printObject(ArrayList<Integer> array){
+        for(int i :array){
+            System.out.print(i+"  ");
+        }
+        System.out.println("");
+    }
+
+    public static void printObjectIndex(ArrayList<Integer> array,int begin ,int end){
+        for(int i=begin;i<=end;i++){
+            System.out.print(array.get(i)+"  ");
+        }
+        System.out.println("");
+    }
+}
