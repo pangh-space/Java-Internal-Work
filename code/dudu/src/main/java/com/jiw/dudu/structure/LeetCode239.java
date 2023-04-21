f8ca587aff8dbb457e07aa11091c1ca4063ac204
pangh <kungfu_star@sina.com>
2023-04-21 10:04:31 +0800

    xxl-job 分布式定时任务Demo

---
+++ code/dudu/src/main/java/com/jiw/dudu/structure/LeetCode239.java
@@ -0,0 +1,19 @@
+package com.jiw.dudu.structure;
+
+/**
+ * @Description LeetCode239
+ * @Author pangh
+ * @Date 2023年04月16日
+ * @Version v1.0.0
+ */
+public class LeetCode239 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // 定义返回结果长度
+        int[] results = new int[nums.length - k -1];
+
+        return results;
+
+    }
+
+}
